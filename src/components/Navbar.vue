da43e551d754378c7f3d845a2718ecf0bedbf61d
thoriqadillah <thoriqadillah59@gmail.com>
2022-05-30 16:07:47 +0700

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+    <div class="navbar-brand">
+      <router-link :to = "{ name: 'index' }" class="navbar-brand">SI PALING VUE JS</router-link>
+    </div>
+
+    <div id="navLink">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link :to="{ name: 'index' }" class="nav-link ">HOME</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'antrian' }" class="nav-link">ANTRIAN</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'poli' }" class="nav-link">POLIKLINIK</router-link>
+          </li>
+        </ul>
+      </div>  
+    </div>
+
+    <div id="auth">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link :to="{ name: 'auth.login' }" class="nav-link">LOGIN</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'auth.register' }" class="nav-link">DAFTAR</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>  
+</div>
+</template>
+
+<script>
+export default {
+  
+  name: 'Navbar',
+}
+</script>
---
+++ src/views/Antrian.vue
@@ -0,0 +1,13 @@
+<template>
+<Navbar></Navbar>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+
+export default {
+    components: {
+      Navbar
+    }
+}
+</script>
