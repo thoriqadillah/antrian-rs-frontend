9fe3166eb9019300d01830fec13be2b18a13b00e
thoriqadillah <thoriqadillah59@gmail.com>
2022-06-02 18:08:35 +0700

    membenahi login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,12 +1,13 @@
 <template>
-<Navbar></Navbar>
+<Navbar :isLoggedin="isLoggedIn"></Navbar>
 <div class="container">
-  <div class="row align-items-center" style="height: 90vh;">
-    <div class="col-8 text-center">
-        <img src="./../../assets/logo.png" alt="logo">
-    </div>
-    <div class="col-4">
-      <FormGenerator :forms="forms" :btn-text="btnText"></FormGenerator>
+  <div class="d-flex justify-content-between align-items-center" style="height: 90vh;">
+    <div></div>
+    <div><img src="./../../assets/logo.png" alt="logo"></div>
+    <div>
+      <form class="px-3 py-5 rounded" @submit.prevent="submit">
+        <FormGenerator :inputs="inputs" :btn-text="btnText"></FormGenerator>
+      </form>
     </div>
   </div>
 </div>
@@ -16,17 +17,30 @@
 import Navbar from "../../components/Navbar.vue";
 import FormGenerator from "../../components/FormGenerator.vue";
 
+import { reactive } from 'vue';
+import axios from 'axios';
+
 export default {
     components: {
       Navbar, FormGenerator
     },
+    setup() {
+      const inputs = reactive([
+          { placeholder: 'Email', value: '', name: 'email' },
+          { placeholder: 'Password', value: '', name: 'password' },
+      ])
+
+      const submit = async () => {
+        const LOGIN_API = 'http://127.0.0.1:8000/api/login'
+        
+      }
+
+      return { inputs, submit }
+    },
     data() {
       return {
-        forms: [
-          { placeholder: 'Username', value: '', name: 'username' },
-          { placeholder: 'Password', value: '', name: 'password' },
-        ],
-        btnText: 'Login'
+        btnText: 'Login',
+        isLoggedIn: false
       }
     },
 }
