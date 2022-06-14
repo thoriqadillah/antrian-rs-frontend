303f41a0a697998cc36f7a069c507ab73b8fb979
thoriqadillah <thoriqadillah59@gmail.com>
2022-06-14 10:59:42 +0700

    wrap container pada admin

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -1,29 +1,32 @@
 <template>
   <NavbarAdmin></NavbarAdmin>
-  <table class="table">
-  <thead>
-      <tr>
-        <th scope="col">Poli</th>
-        <th scope="col">Nomor Antrian</th>
-        <th scope="col">Edit</th>
-      </tr>
-  </thead>
-  <tbody>
-    <tr v-for="poli, index in polis" :key="poli">
-      <th scope="row">{{ poli.nama_poli }}</th>
-      <td>{{ nomor[index].nomor }}</td>
-      <td>
-            <button
-              type="button"
-              class="btn btn-success"
-              @click="selesai"
-              :data-id="index+1">
-              Selesai
-            </button>
-      </td>
-    </tr>
-  </tbody>
-</table>
+  <div class="container">
+    <table class="table">
+      <thead>
+          <tr>
+            <th scope="col">Poli</th>
+            <th scope="col">Nomor Antrian</th>
+            <th scope="col">Edit</th>
+          </tr>
+      </thead>
+      <tbody>
+        <tr v-for="poli, index in polis" :key="poli">
+          <th scope="row">{{ poli.nama_poli }}</th>
+          <td>{{ nomor[index].nomor }}</td>
+          <td>
+                <button
+                  type="button"
+                  class="btn btn-success"
+                  @click="selesai"
+                  :data-id="index+1">
+                  Selesai
+                </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>  
+  </div>
+  
 </template>
 <script>
 
